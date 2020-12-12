dee26da31f300afa3f5336c999daf40adf0d9a9e
Ferry Kurniawan <ferryku8@gmail.com>
2020-12-12 14:18:25 +0700

    TUGAS 3 C++
    Mengumpulkan Tugas 3 C++

---
+++ Tugas 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int A,B,AB;
+	A = 10;
+	B = 20;
+	AB = A*B;
+	cout<<"Nilai Variabel A : " <<A<<endl;
+	cout<<"Nilai Variabel B : "<<B<<endl;
+	cout<<"Hasil Perkalian Kedua Variabel Adalah : "<<AB<<endl;	
+}
